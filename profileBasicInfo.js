1ad6499e7a68536bf4987b6ece58c18010431e40
JsdCloud <jsdavi21@asu.edu>
2023-04-13 15:25:03 -0700

    setting up profiles js

---
+++ profileBasicInfo.js
@@ -0,0 +1,52 @@
+
+
+
+function update(){
+    console.log("page load");
+
+    //called when you load page
+
+    // load_profile
+
+}
+
+
+function load_profile(){
+
+    // get email
+    // restful call /users/:user_id/email?key=val
+
+    // get last name
+    // restful call
+
+    // get first Name
+    // restful call
+
+    // get company
+    // restful call
+
+    // get phone num
+    // restful call
+
+}
+
+function save_profile(){
+
+    console.log("in save profile");
+
+    let form = document.getElementById("profile");
+
+    let email = form.email.value;
+    console.log("email found: " + email);
+    //let lastName = form.lastName.value;
+
+    // get last name
+
+    // get first Name
+
+    // get company (if there)
+
+    // get phone number (if there)
+
+}
+
