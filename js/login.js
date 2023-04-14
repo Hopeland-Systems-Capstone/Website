a1e90f3f9319e055061854976b51938f02f37d7e
jacob <jacob@stellardev.org>
2023-04-13 19:48:14 -0700

    Backend connection error checking

--- js/login.js
+++ js/login.js
@@ -6,14 +6,15 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
 
     const user_id = await getUserIdByEmail(email);
 
+    if (user_id === -2) return;
     if (user_id === -1) {
         alert('Incorrect email or password!');
         return;
     }
-
+    
     const hashed_password = await hashPassword(password);
     const isPasswordCorrect = await checkPassword(user_id, hashed_password);
-
+    
     if (isPasswordCorrect) {
         login(user_id);
         window.location.href = "main.html";
@@ -24,14 +25,24 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
 });
   
 async function getUserIdByEmail(email) {
-    const response = await fetch(`http://localhost:3000/users/email/${email}?key=9178ea6e1bfb55f9a26edbb1f292e82d`);
-    return await response.json();
-}
+    try {
+        const response = await fetch(`http://localhost:3000/users/email/${email}?key=9178ea6e1bfb55f9a26edbb1f292e82d`);
+        return await response.json();
+    } catch (error) {
+        alert("Error connecting. Try again later.");
+        return -2;
+    }
+  }
 
 async function checkPassword(user_id, hashed_password) {
-    const response = await fetch(`http://localhost:3000/users/${user_id}/password/${hashed_password}?key=9178ea6e1bfb55f9a26edbb1f292e82d`);
-    const isPasswordCorrect = await response.json();
-    return isPasswordCorrect;
+    try {
+        const response = await fetch(`http://localhost:3000/users/${user_id}/password/${hashed_password}?key=9178ea6e1bfb55f9a26edbb1f292e82d`);
+        const isPasswordCorrect = await response.json();
+        return isPasswordCorrect;
+    } catch (error) {
+        alert("Error connecting. Try again later.");
+        return false;
+    }
 }
   
 async function hashPassword(password) {
