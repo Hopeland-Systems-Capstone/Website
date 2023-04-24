48b062d19aafd42a0f47d6bedf0f7dbc146695c6
Chris Swanson <cswanso9@asu.edu>
2023-04-24 15:40:42 -0700

    Added dynamic charts to dashboard

--- public/js/map.js
+++ public/js/map.js
@@ -1,46 +1,64 @@
-var map = L.map('map').setView([34.5, -112], 8);
+var map = L.map('map').setView([33.5, -112.5], 9);
 
-sensorData = [];
+const initialize = async () => {
+  //get width and height of map in degrees of longitude and latitude
+  var width = map.getBounds().getEast() - map.getBounds().getWest()+0.5;
+  var height = map.getBounds().getNorth() - map.getBounds().getSouth()+0.5;
+  var center = map.getCenter();
 
-//temporary data structure for a sensor
-function createSensorData(id, lat, long, name, type, onlineStatus) {
-  return {
-    id: id,               // --unique key
-    latitude: lat,        // --coordinates of sensor
-    longitude: long,      // 
-    name: name,           // --descriptive name of sensor location
-    type: type,
-    onlineStatus: onlineStatus,
-    temperature: 70,      // --temperature in farenheit
-    humidity: .1,         // --humidity; .1 = 10%
-    co2: 300              // --carbon dioxide levels in parts per million
-  }
+  const METERS_IN_ONE_DEGREE = 111111;
+
+  //calculate distance in meters from center of map to edge of map in each direction
+  var latRange = Math.abs(METERS_IN_ONE_DEGREE*height/2);
+  var longRange = Math.abs(METERS_IN_ONE_DEGREE*Math.cos(center.lat)*width/2);
+
+  //calculate distance in meters from center of map to corner of map
+  var range = Math.sqrt(latRange * latRange + longRange * longRange)
+
+  const response = await fetch(`http://localhost:3000/sensors?key=9178ea6e1bfb55f9a26edbb1f292e82d&longitude=${center.lng}&latitude=${center.lat}&distance=${range}`);
+  const sensors = await response.json();
+
+  sensors.forEach(async sensor => {
+    createMarker(sensor);
+  });
 }
 
 //accepts sensor data structure and creates a marker on the map for that sensor
 function createMarker(sensor) {
 
-  //Marker Icon selection;
-  //There's probably a more elegant way to do this.
-  var sensorIcon;
-  if (sensor.onlineStatus) {
-    if (sensor.type == "Forest") sensorIcon = forestOnlineIcon;
-    if (sensor.type == "Flood") sensorIcon = floodOnlineIcon;
-    if (sensor.type == "Gateway") sensorIcon = gatewayOnlineIcon;
-  }
-  else {
-    if (sensor.type == "Forest") sensorIcon = forestOfflineIcon;
-    if (sensor.type == "Flood") sensorIcon = floodOfflineIcon;
-    if (sensor.type == "Gateway") sensorIcon = gatewayOfflineIcon;
-  }
+  var latlng;
 
-  var marker = new customMarker([sensor.latitude, sensor.longitude], {icon: sensorIcon}).addTo(map);
-  var popuptext = markerMouseOverGenerate(sensor);
-  var popup = new L.popup({ offset:[0,15] }).setContent(popuptext);
-  marker.bindPopup(popup, { showOnMouseOver: true });
+  try {
+    
+    //latitude and longitude are swapped between API and Leaflet
+    var temp_latlng = sensor.geolocation.coordinates;
+    latlng = [temp_latlng[1],temp_latlng[0]]
 
-  return marker;
+    var sensorIcon;
+    if (sensor.status === "Online") {
+      if (sensor.type == "Forest") sensorIcon = forestOnlineIcon;
+      if (sensor.type == "Flood") sensorIcon = floodOnlineIcon;
+      if (sensor.type == "Gateway") sensorIcon = gatewayOnlineIcon;
+    }
+    else {
+      if (sensor.type == "Forest") sensorIcon = forestOfflineIcon;
+      if (sensor.type == "Flood") sensorIcon = floodOfflineIcon;
+      if (sensor.type == "Gateway") sensorIcon = gatewayOfflineIcon;
+    }
+
+    var marker = new customMarker(latlng, {icon: sensorIcon}).addTo(map);
+    var popuptext = markerMouseOverGenerate(sensor);
+    var popup = new L.popup({ offset:[0,15] }).setContent(popuptext);
+    marker.bindPopup(popup, { showOnMouseOver: true, maxWidth: 500 });
+
+    return marker;
+  }
+  catch (error) {
+    console.log(error);
+  }
 }
+
+map.on('moveend', initialize);
 
 //In Leaflet, Tooltips that appear when hovering over Markers work a bit stupidly: 
 //Moving your mouse ONTO the Tooltip causes it to go away. This customMarker extention of the default
@@ -110,7 +128,7 @@ var DeviceIcon = L.Icon.extend({
   options: {
     iconSize: [37,68],
     iconAnchor: [17,60],
-    popupAnchor: [17,120]
+    popupAnchor: [17,-10]
   }
 });
 
@@ -122,39 +140,44 @@ var forestOnlineIcon = new DeviceIcon({iconUrl: './images/map/forest-online.png'
     gatewayOfflineIcon = new DeviceIcon({iconUrl: './images/map/gateway-offline.png'});
 
 
-//Six sensors to make sure things are good.
-var tempSensor1 = createSensorData(120, 34.5, -112, "FR 117", "Forest", true)
-var tempMarker1 = createMarker(tempSensor1);
-
-var tempSensor2 = createSensorData(120, 34, -112.5, "FR 118", "Forest", false)
-var tempMarker2 = createMarker(tempSensor2);
-
-var tempSensor3 = createSensorData(120, 35, -112.5, "FR 117", "Flood", true)
-var tempMarker3 = createMarker(tempSensor3);
-
-var tempSensor4 = createSensorData(120, 35.5, -112.5, "FR 118", "Flood", false)
-var tempMarker4 = createMarker(tempSensor4);
-
-var tempSensor5 = createSensorData(120, 35.5, -113, "FR 117", "Gateway", true)
-var tempMarker5 = createMarker(tempSensor5);
-
-var tempSensor6 = createSensorData(120, 34.5, -113, "FR 118", "Gateway", false)
-var tempMarker6 = createMarker(tempSensor6);
-
 function markerMouseOverGenerate(sensor) {
+  const co2 = Math.round(sensor.co2.at(-1).value);
+  const temperature = Math.round(sensor.temperature.at(-1).value*100)/100;
+  const humidity = Math.round(sensor.humidity.at(-1).value*10)/10;
+  const pressure = Math.round(sensor.pressure.at(-1).value);
+
   return (
     `<div class='markerHoverBox'>
       <div class='markerHoverBoxDeviceName'>
-        ${sensor.name} - ${sensor.type}
+        <h5>${sensor.name} - ${sensor.type}</h5>
       </div>
       <div class='markerHoverBoxDeviceInfo'>
         <div class='markerHoverBoxDeviceStatus'>
-          test text 
+          <b>Interface Status</b><br>
+          CO2: <span class="device-detail">${co2} ppm</span><br>
+          Temperature: <span class="device-detail">${temperature}C </span><br>
+          Humidity: <span class="device-detail">${humidity}%</span><br>
+          Barometric Pressure: <span class="device-detail">${pressure} hPa</span><br>
         </div>
         <div class='markerHoverBoxDeviceDetails'>
-          test text
+          <b>Device Information</b><br>
+          RSSI: <span class="device-detail">-63 dBm</span><br>
+          SNR: <span class="device-detail">14 dB</span><br>
+          SN: <span class="device-detail">178FJWO9SKLL8</span><br>
+          SF: <span class="device-detail">7</span><br>
+          Battery: <span class="device-detail">100%</span><br>
+          Model: <span class="device-detail">WS101</span><br>
+          IMEI: <span class="device-detail">-</span><br>
+          Firmware: <span class="device-detail">-</span><br>
+          Group Name: <span class="device-detail">-</span><br>
+          Hardware: <span class="device-detail">-</span><br>
+          Device ID: <span class="device-detail">-</span><br>
+          Associated Gateway: <span class="device-detail">-</span><br>
+          Device EUI: <span class="device-detail">-</span><br>
         </div>
       </div>
     </div>`
   );
 }
+
+window.onload = initialize;
