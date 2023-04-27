63236623eff4af701f4a4538a61f0ba9241ef4ad
jacob <jacob@stellardev.org>
2023-04-27 01:20:18 -0700

    Cleanup URLs and add favicon

--- app.js
+++ app.js
@@ -11,11 +11,33 @@ app.use(bodyParser.json());
 app.use(authMiddleware);
 app.use(express.static('public', { index: 'index.html' }));
 
+const routeMap = {
+    '/index.html': '/login',
+    '/main.html': '/dashboard',
+    '/gateways.html': '/gateways',
+    '/sensors.html': '/sensors',
+    '/map.html': '/map',
+    '/reports.html': '/reports',
+    '/profileBasicInfo.html': '/profile',
+    '/profileBillingInfo.html': '/billing',
+    '/profileBillingHistory.html': '/billing-history',
+    '/profileDateTime.html': '/time',
+    '/profileChangePassword.html': '/password',
+    '/profileAlarmRecipients.html': '/alarm-recipients'
+};
+
 async function authMiddleware(req, res, next) {
     if (req.path.endsWith('.html')) {
         try {
             await auth_api.checkLoggedIn(req, res);
-            next();
+            // Remove the .html extension from the URL
+            const url = routeMap[req.path];
+            if (url) {
+                res.redirect(url);
+            } else {
+                console.log("NOO URL FOUNDDD url");
+                next();
+            }
         } catch (err) {
             res.redirect(err.message);
         }
@@ -27,5 +49,12 @@ async function authMiddleware(req, res, next) {
 //Routes
 app.use("/", require("./routes/auth.js"));
 app.use("/", require("./routes/data.js"));
+
+
+for (const htmlFile in routeMap) {
+    app.get(routeMap[htmlFile], (req, res) => {
+        res.sendFile(__dirname + `/public/${htmlFile}`);
+    });
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
